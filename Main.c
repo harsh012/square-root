f5fa424967a8ed040208e50c266bf14b6e5c21e9
harsh012 <49400773+harsh012@users.noreply.github.com>
2019-06-26 20:26:11 +0530

    Create Main.c

---
+++ Main.c
@@ -0,0 +1,12 @@
+#include  <stdio.h>
+#include <conio.h>
+#include <math.h>
+void main ()
+{
+int a,b;
+printf ("enter the number ");
+scanf ("%d",&a);
+b=sqrt (a);
+printf ("square root",a);
+getch ();
+}
